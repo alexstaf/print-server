b2b71689cd5405f5fde4f3eac3b22e7d763987e3
alexstaf <alexstaf1@gmail.com>
2020-12-29 00:27:32 +0300

    Fix debug mode.

--- print_server/serve.py
+++ print_server/serve.py
@@ -48,4 +48,4 @@ def parse_args():
 
 if __name__ == '__main__':
     args = parse_args()
-    app.run(host='127.0.0.1', port=args.port, debug=True)
+    app.run(host='127.0.0.1', port=args.port, debug=args.debug)
