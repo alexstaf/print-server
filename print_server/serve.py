8e6cab7788f621cf093f7491afb99d64b1a050fa
alexstaf <alexstaf1@gmail.com>
2021-01-03 23:42:01 +0300

    Add address to arguments.

--- print_server/serve.py
+++ print_server/serve.py
@@ -55,6 +55,8 @@ def parse_args():
     parser.add_argument('-h', '--help', action='help',
                         default=argparse.SUPPRESS,
                         help='Show this help message and exit.')
+    parser.add_argument('-a', '--address', default='127.0.0.1',
+                        help='Set address to use.')
     parser.add_argument('-p', '--port', type=int, default=8000,
                         help='Set server port.')
     parser.add_argument('-d', '--debug', action='store_true',
@@ -66,7 +68,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    app.run(host='127.0.0.1', port=args.port, debug=args.debug)
+    app.run(host=args.address, port=args.port, debug=args.debug)
 
 
 if __name__ == '__main__':
