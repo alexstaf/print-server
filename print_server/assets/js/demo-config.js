dbb4651d78d611c8fe66adb304a6f1c1063179d9
alexstaf <alexstaf1@gmail.com>
2021-01-03 03:23:15 +0300

    Clean up the code.

--- print_server/assets/js/demo-config.js
+++ print_server/assets/js/demo-config.js
@@ -1,10 +1,4 @@
 $(function(){
-  /*
-   * For the sake keeping the code clean and the examples simple this file
-   * contains only the plugin configuration & callbacks.
-   * 
-   * UI functions ui_* can be located in: demo-ui.js
-   */
   $('#drag-and-drop-zone').dmUploader({ //
     url: '/upload',
     dataType: 'text',
@@ -22,20 +16,16 @@ $(function(){
     },
     onInit: function(){
       // Plugin is ready to use
-      //ui_add_log('Penguin initialized :)', 'info');
     },
     onComplete: function(){
       // All files in the queue are processed (success or error)
-      //ui_add_log('All pending tranfers finished');
     },
     onNewFile: function(id, file){
       // When a new file is added using the file selector or the DnD area
-      //ui_add_log('New file added #' + id);
       ui_multi_add_file(id, file);
     },
     onBeforeUpload: function(id){
       // about tho start uploading a file
-      //ui_add_log('Starting the upload of #' + id);
       ui_multi_update_file_status(id, 'uploading', 'Uploading...');
       ui_multi_update_file_progress(id, 0, '', true);
     },
@@ -50,8 +40,6 @@ $(function(){
     },
     onUploadSuccess: function(id, data){
       // A file was successfully uploaded
-      //ui_add_log('Server Response for file #' + id + ': ' + JSON.stringify(data));
-      //ui_add_log('Upload of file #' + id + ' COMPLETED', 'success');
       ui_multi_update_file_status(id, 'success', 'Upload Complete');
       ui_multi_update_file_progress(id, 100, 'success', false);
     },
@@ -61,10 +49,9 @@ $(function(){
     },
     onFallbackMode: function(){
       // When the browser doesn't support this plugin :(
-      //ui_add_log('Plugin cant be used here, running Fallback callback', 'danger');
     },
     onFileSizeError: function(file){
-      //ui_add_log('File \'' + file.name + '\' cannot be added: size excess limit', 'danger');
+      // onFileSizeError
     }
   });
 });
