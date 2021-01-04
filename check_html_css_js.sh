0565790bc7c3af931e77a6c0407d1333f073cdb3
alexstaf <alexstaf1@gmail.com>
2021-01-04 23:50:55 +0300

    Fix JS errors.

--- check_html_css_js.sh
+++ check_html_css_js.sh
@@ -1,4 +1,3 @@
 htmlhint print_server/templates/index.html
 stylelint print_server/assets/css/main.css
-eslint print_server/assets/js/ui.js
-eslint print_server/assets/js/config.js
+eslint print_server/assets/js/main.js
