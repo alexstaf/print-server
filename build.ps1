145a6d8d438bac9b7d3c7257693682ca6182aef8
alexstaf <alexstaf1@gmail.com>
2021-11-10 12:19:28 +0300

    Improve workflow caching during build stage.

--- build.ps1
+++ build.ps1
@@ -1,20 +1,5 @@
-mkdir python3.8.10
-cd python3.8.10
-cp ..\python3.8.10.zip python3.8.10.zip
-7z x python3.8.10.zip
-cp ..\get-pip.py get-pip.py
-echo "import site" | out-file -append -encoding ASCII python38._pth
-.\python get-pip.py
-pip install --target Lib\site-packages --python-version 3.8.10 --platform win32 --no-deps -r ..\requirements.txt
-pip install --target Lib\site-packages --python-version 3.8.10 --platform win32 --no-deps --ignore-requires-python ..
-mv python38.zip python3.8.zip
-mkdir python38.zip
-mv python3.8.zip python38.zip/python38.zip
-cd python38.zip
-7z x python38.zip
-cd ..
-cp ..\pyinstaller\* .\
-echo ".\python -m $(cat make_exe.bat)" > build_package.ps1
-.\build_package.ps1
+cd python3_embed
+.\build_app.ps1
 mv dist\run_server dist\print_server
 mv dist ..\
+cd ..
---
+++ prepare.ps1
@@ -0,0 +1,22 @@
+rm -r -ErrorAction Ignore python3_embed
+mkdir python3_embed
+cd python3_embed
+cp ..\python3_embed.zip python3_embed.zip
+7z x python3_embed.zip
+rm python3_embed.zip
+cp ..\get-pip.py get-pip.py
+echo "import site" | out-file -append -encoding ASCII python38._pth
+.\python get-pip.py
+rm get-pip.py
+pip install --target Lib\site-packages --python-version 3.8.10 --platform win32 --no-deps -r ..\requirements.txt
+pip install --target Lib\site-packages --python-version 3.8.10 --platform win32 --no-deps --ignore-requires-python ..
+mv python38.zip python3.8.zip
+mkdir python38.zip
+mv python3.8.zip python38.zip/python38.zip
+cd python38.zip
+7z x python38.zip
+rm python38.zip
+cd ..
+cp ..\pyinstaller\* .\
+echo ".\python -m $(cat make_exe.bat)" > build_app.ps1
+cd ..
