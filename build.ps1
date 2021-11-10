1fb170984fb275c235613166793155ffe48aa466
alexstaf <alexstaf1@gmail.com>
2021-11-10 13:14:30 +0300

    Workflow: Add building & saving one file application.

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ cd python3_embed
 rm -r -ErrorAction Ignore build
 .\build_app.ps1
 mv dist\run_server dist\print_server
+mv dist\run_server.exe dist\print_server.exe
 mv dist ..\
 rm -r build
 cd ..
---
+++ pyinstaller/make_portable.bat
@@ -0,0 +1,1 @@
+PyInstaller --noconfirm --onedir --console --add-data "../print_server/assets;data/assets" --add-data "../print_server/templates;data/templates" run_server.py
