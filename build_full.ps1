738b60487c7f95a93561a08e1c5967f860d389e6
alexstaf <alexstaf1@gmail.com>
2021-11-14 14:02:48 +0300

    Add scripts to build application (full) and clear all build data.

---
+++ build_full.ps1
@@ -0,0 +1,5 @@
+.\clear_all.ps1
+.\download.ps1
+.\generate_requirements.ps1
+.\prepare.ps1
+.\build.ps1
---
+++ clear_all.ps1
@@ -0,0 +1,6 @@
+rm -r -ErrorAction Ignore build
+rm -r -ErrorAction Ignore dist
+rm -r -ErrorAction Ignore python3_embed
+rm -ErrorAction Ignore get-pip.py
+rm -ErrorAction Ignore python3_embed.zip
+rm -ErrorAction Ignore requirements.txt
